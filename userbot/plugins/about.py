021d603ee4f80282d34871d855cf6b6a4944936b
VISHNU CS <58348499+vishnu175@users.noreply.github.com>
2020-09-12 01:41:30 +0530

    Update about.py

--- userbot/plugins/about.py
+++ userbot/plugins/about.py
@@ -1,6 +1,6 @@
-# Ported from other Telegram UserBots for TeleBot//Made for TeleBot
+# Ported from other Telegram UserBots for SPARKZZZ//Made for SPARKZZZ
 # Kangers, don't remove this line 
-# @its_xditya
+# @its_vishnu175
 
 """Available Commands:
 .info
@@ -10,17 +10,17 @@ import asyncio
 
 from userbot.utils import admin_cmd
 
-@telebot.on(admin_cmd(pattern="info"))
+@sparkzzz.on(admin_cmd(pattern="info"))
 async def _(event):
     if event.fwd_from:
         return
     animation_interval = 0.1
     animation_ttl = range(0,36)
     #input_str = event.pattern_match.group(1)
-   # if input_str == "Visit this page to know more about TeleBot.":
+   # if input_str == "Visit this page to know more about SPARKZZZ.":
     await event.edit("Thanks")
     animation_chars = [
-            "**TeleBot**",
+            "**⚡SPARKZZZ ⚡**",
             "[More Info](https://telegra.ph/TeleBot-07-08)"
          ]
             
