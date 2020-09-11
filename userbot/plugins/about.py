8a54f37293157e9b9c5280281c6eb6738f29d858
VISHNU CS <58348499+vishnu175@users.noreply.github.com>
2020-09-12 02:15:17 +0530

    Update about.py

--- userbot/plugins/about.py
+++ userbot/plugins/about.py
@@ -9,8 +9,9 @@
 import asyncio
 
 from userbot.utils import admin_cmd
+sparkzzz = bot 
 
-@sparkzzz.on(admin_cmd(pattern="info"))
+@ sparkzzz.on(admin_cmd(pattern="info"))
 async def _(event):
     if event.fwd_from:
         return
