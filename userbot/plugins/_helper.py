8db51cb7575165176a9359bf62f2271dc7e0684f
VISHNU CS <mailvishnucs@gmail.com>
2020-09-12 12:40:18 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -25,7 +25,7 @@ async def cmd_list(event):
             if len(string) > 4095:
                 with io.BytesIO(str.encode(string)) as out_file:
                     out_file.name = "cmd.txt"
-                    await bot.send_file(u
+                    await bot.send_file(
                         event.chat_id,
                         out_file,
                         force_document=True,
