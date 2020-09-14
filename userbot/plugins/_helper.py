6080e9f6136bf0b81dcd8a7d0ae6e47306ed2163
VISHNU CS <mailvishnucs@gmail.com>
2020-09-14 11:45:50 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,5 +1,6 @@
 from userbot import CMD_LIST
-
+from userbot import ALIVE_NAME
+from userbot.utils import admin_cmd
 @command(pattern="^.help ?(.*)")
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
