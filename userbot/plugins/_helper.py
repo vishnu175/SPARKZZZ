f425b4654e6c4c642506d41c2ef6c1521128ea02
VISHNU CS <mailvishnucs@gmail.com>
2020-09-14 08:19:36 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -47,10 +47,10 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-                 help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[SPARKZZZ BOT]**\n\n"""
-            
-                tgbotusername,
-                help_string
+                   help_string = """SPARKZZZ Userbot Modules of my Boss  Are Listed Here !"""""
+                   results = await bot.inline_query(  # pylint:disable=E0602
+                   tgbotusername,
+                   help_string
             )
             await results[0].click(
                 event.chat_id,
