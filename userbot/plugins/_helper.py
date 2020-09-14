944def0015c0ed8366c388cdd51452f07cea9f21
VISHNU CS <mailvishnucs@gmail.com>
2020-09-14 13:11:02 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -5,11 +5,11 @@
 from userbot import CMD_LIST
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
-sparkzzbot = bot 
+
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SPARKZZZ User"
 
-@ sparkzzzbot.on(admin_cmd(pattern="help ?(.*)"))
+@telebot.on(admin_cmd(pattern="help ?(.*)"))
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
