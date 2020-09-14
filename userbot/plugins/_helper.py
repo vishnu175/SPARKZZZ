cb62ad5e4bb46a2a8d1124232821c34111e6bc96
VISHNU CS <mailvishnucs@gmail.com>
2020-09-14 09:41:16 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -47,7 +47,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-                   help_string = f"""SPARKZZZ modules are listed here! Type .help(module name) to know about how its working \n\n"""
+                   help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[SPARKZZZ](https://github.com/vishnu175/SPARKZZZ/)**\n\n"""
                    results = await bot.inline_query(  # pylint:disable=E0602
                    tgbotusername,
                    help_string
@@ -58,5 +58,5 @@ async def cmd_list(event):
                 hide_via=True
             )
             await event.delete()
-            
+
             
