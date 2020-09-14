c2c65d404d9a31194922973a78bb90f8353ac6c9
VISHNU CS <mailvishnucs@gmail.com>
2020-09-14 12:52:05 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,14 +1,14 @@
-# Ported from other Telegram UserBots for TeleBot
+# Ported from other Telegram UserBots for SPARKZZZ
 # Kangers, don't remove this line 
-# @its_xditya
+# @csv1990
 
 from userbot import CMD_LIST
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TeleBot User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SPARKZZZ User"
 
-@telebot.on(admin_cmd(pattern="help ?(.*)"))
+@bot.on(admin_cmd(pattern="help ?(.*)"))
 async def cmd_list(event):
     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
@@ -41,12 +41,12 @@ async def cmd_list(event):
                 for i in CMD_LIST[input_str]:
                     string += "    " + i
                     string += "\n"
-                string +="\n**© @TeleBotSupport**"
+                string +="\n**© @sparkzzzbotsupport**"
                 await event.edit(string)
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://xditya.gitbook.io/telebot/)**\n\n"""
+            help_string = f"""Showing all the modules of **[SPARKZZZ](https://github.com/vishnu175/SPARKZZZ/)**\n\n"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
