01c39e16b88a1681ca099b66ee6a29e4a686117f
VISHNU CS <mailvishnucs@gmail.com>
2020-09-15 18:46:23 +0530

    Update carbon.py

--- userbot/plugins/carbon.py
+++ userbot/plugins/carbon.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 # Licensed under the Raphielscape Public License, Version 1.b (the "License");
 # you may not use this file except in compliance with the License.
-# @TeleBotSupport
+# @Telebot
 
 """ Userbot module containing various scrapers. """
 import os
