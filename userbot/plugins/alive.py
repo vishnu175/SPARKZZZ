6a729309252aece39a7789983504591f0bb74193
VISHNU CS <mailvishnucs@gmail.com>
2020-09-17 23:35:07 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -74,7 +74,7 @@ async def amireallyalive(alive):
         await borg.send_file(alive.chat_id, ALV_PIC,caption=tele, link_preview = False)
         await alive.delete()
         return
-    req = requests.get("https://telegra.ph/file/4588adc70e2b4aa8242a8.jpg")
+    req = requests.get("https://telegra.ph/file/2fe25727349e5ec1c035c.png")
     req.raise_for_status()
     file = BytesIO(req.content)
     file.seek(0)
