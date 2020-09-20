a90ba75285d0eec64b204734378443fa45fd547f
VISHNU CS <mailvishnucs@gmail.com>
2020-09-21 01:01:42 +0530

    Create idk.py

---
+++ userbot/plugins/idk.py
@@ -0,0 +1,29 @@
+@ copyright SPARKZZZ
+
+"""
+
+idk
+
+"""
+
+
+
+from telethon.tl.functions.channels import LeaveChannelRequest
+from userbot.utils import admin_cmd
+import time
+
+@sparkzzz.on(admin_cmd(pattern="idk", outgoing=True))
+
+async def leave(e):
+
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+
+        await e.edit("`I dont know it exactly bro...😂😂😂.`")
+
+        time.sleep(3)
+
+        if '-' in str(e.chat_id):
+
+            await sparkzzz(LeaveChannelRequest(e.chat_id))
+
+        
