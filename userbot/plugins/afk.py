fe2288c3de0c34ca304bdc67fd1988b7381a6c68
VISHNU CS <mailvishnucs@gmail.com>
2020-09-22 00:17:04 +0530

    Update afk.py

--- userbot/plugins/afk.py
+++ userbot/plugins/afk.py
@@ -49,7 +49,7 @@ async def _(event):
             afk_time = datetime.datetime.now()  # pylint:disable=E0602
         USER_AFK = f"yes: {reason}"  # pylint:disable=E0602
         if reason:
-            await borg.send_message(event.chat_id, f"**I shall be Going afk!** __because ~ {reason}__")
+            await borg.send_message(event.chat_id, f"**I am Going afk!** __Reason - {reason}__")
         else:
             await borg.send_message(event.chat_id, f"**I am Going afk!**")
         await asyncio.sleep(5)
