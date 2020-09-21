31a9ef6cef4ccc6afea73427029ca5e2e4b7c374
VISHNU CS <mailvishnucs@gmail.com>
2020-09-21 20:05:55 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -175,9 +175,8 @@ if bool(ENV):
     G_PHOTOS_CLIENT_ID = os.environ.get("G_PHOTOS_CLIENT_ID", None)
     G_PHOTOS_CLIENT_SECRET = os.environ.get("G_PHOTOS_CLIENT_SECRET", None)
     G_PHOTOS_AUTH_TOKEN_ID = os.environ.get("G_PHOTOS_AUTH_TOKEN_ID", None)
-    if G_PHOTOS_AUTH_TOKEN_ID:
-        G_PHOTOS_AUTH_TOKEN_ID = int(G_PHOTOS_AUTH_TOKEN_ID)
-
+    TEMP_DOWNLOAD_DIRECTORY = os.environ.get("TEMP_DOWNLOAD_DIRECTORY",
+                                         "./downloads")
 
     # Google Drive Module
     G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID", None)
