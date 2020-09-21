a3a381d98f9d3c5ab94cfb69267ca81390597172
VISHNU CS <mailvishnucs@gmail.com>
2020-09-22 02:13:50 +0530

    Update emojigames.py

--- userbot/plugins/emojigames.py
+++ userbot/plugins/emojigames.py
@@ -93,6 +93,6 @@ async def _(event):
 
 CMD_HELP.update({
     "emojigames":
-    "`.dice` 1-6 or `.dart`1-6 or `.bb`1-5 or `.fb`1-5\
+    "`.dice` 1-6 or `.dart`1-6 or `.bb`1-5 or `.fb`1-5 or `.arrow`1-5\
 \nUsage: hahaha just a magic.\nWarning:`Don't use any other values or bot will crash`"
 })
