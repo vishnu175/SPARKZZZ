fea31e993f64fb3bc72fdda05b05bf6658854fc2
VISHNU CS <mailvishnucs@gmail.com>
2020-09-23 12:20:22 +0530

    Delete __init__.py

--- userbot/helpers/__init__.py
+++
@@ -1,6 +0,0 @@
-from . import emojify, fonts
-from .exceptions import CancelProcess
-from .functions import *
-from .memeifyhelpers import *
-from .progress import progress
-from .qhelper import process
