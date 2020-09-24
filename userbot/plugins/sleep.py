f27675883fd7b177e93bdb7104d7c025805195d8
VISHNU CS <mailvishnucs@gmail.com>
2020-09-24 14:53:18 +0530

    Update sleep.py

--- userbot/plugins/sleep.py
+++ userbot/plugins/sleep.py
@@ -34,7 +34,7 @@ async def set_not_afk(event):
                 event.chat_id,
                 "Please set `PLUGIN_CHANNEL` " + \
                 "for the proper functioning of afk functionality " + \
-                "in @FridayOT\n\n `{}`".format(str(e)),
+                "in @sparkzzzbothelp\n\n `{}`".format(str(e)),
                 reply_to=event.message.id,
                 silent=True
             )
