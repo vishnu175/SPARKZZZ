b461ee369fb04f5dc0459c20449e89b5d2f6ccc9
VISHNU CS <mailvishnucs@gmail.com>
2020-09-30 04:11:35 +0530

    Create inline_fun.py

---
+++ userbot/plugins/inline_fun.py
@@ -0,0 +1,55 @@
+from userbot.utils import admin_cmd, sudo_cmd, edit_or_reply
+from var import Var
+
+
+
+
+
+@borg.on(admin_cmd(pattern="stat$"))
+async def stats(event):
+    if event.fwd_from:
+        return
+    botusername = Var.TG_BOT_USER_NAME_BF_HER
+    noob = "stats"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+
+@borg.on(admin_cmd(pattern="xogame$"))
+async def gamez(event):
+    if event.fwd_from:
+        return
+    botusername = "@xobot"
+    noob = "play"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, noob) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+@borg.on(admin_cmd(pattern="wspr ?(.*)"))
+async def wspr(event):
+    if event.fwd_from:
+        return
+    wwwspr = event.pattern_match.group(1)
+    botusername = "@whisperBot"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, wwwspr) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
+
+@borg.on(admin_cmd(pattern="mod ?(.*)"))
+async def mod(event):
+    if event.fwd_from:
+        return
+    modr = event.pattern_match.group(1)
+    botusername = "@PremiumAppBot"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    tap = await bot.inline_query(botusername, modr) 
+    await tap[0].click(event.chat_id)
+    await event.delete()
