9d0ba560b78347cd65a96667b3b5571e82fc7bf6
VISHNU CS <mailvishnucs@gmail.com>
2020-10-03 12:22:19 +0530

    updater for devolop branch

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -82,12 +82,12 @@ async def upstream(ups):
         origin = repo.create_remote('upstream', off_repo)
         origin.fetch()
         force_updateme = True
-        repo.create_head('master', origin.refs.master)
-        repo.heads.master.set_tracking_branch(origin.refs.master)
-        repo.heads.master.checkout(True)
+        repo.create_head('devolop', origin.refs.devolop)
+        repo.heads.devolop.set_tracking_branch(origin.refs.devolop)
+        repo.heads.devolop.checkout(True)
 
     ac_br = repo.active_branch.name
-    if ac_br != 'master':
+    if ac_br != 'devolop':
         await ups.edit(
             f'**[UPDATER]:**` Looks like you are using your own custom branch ({ac_br}). '
             'in that case, Updater is unable to identify '
@@ -167,7 +167,7 @@ async def upstream(ups):
         else:
             remote = repo.create_remote("heroku", heroku_git_url)
         try:
-            remote.push(refspec="HEAD:refs/heads/master", force=True)
+            remote.push(refspec="HEAD:refs/heads/devolop", force=True)
         except GitCommandError as error:
             await ups.edit(f'{txt}\n`Here is the error log:\n{error}`')
             repo.__del__()
