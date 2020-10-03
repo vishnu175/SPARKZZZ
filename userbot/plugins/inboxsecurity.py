546c5919383baaaf4d412fcc6117a9a8f15c6a1f
VISHNU CS <mailvishnucs@gmail.com>
2020-10-03 13:31:33 +0530

    Update inboxsecurity.py

--- userbot/plugins/inboxsecurity.py
+++ userbot/plugins/inboxsecurity.py
@@ -22,8 +22,9 @@ DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SPARKZZZ User"
 USER_BOT_WARN_ZERO = "`Hey you,..I have already warned you not to spam inbox ✉️. Now you have been blocked and reported until further notice.`\n\n**GoodBye🙋!** "
 USER_BOT_NO_WARN = ("**Welcome to ⚡SPARKZZZ inbox security 🔐.**\n\nNice to see you here.unfortunately  "
                     f"[{DEFAULTUSER}](tg://user?id={myid}) is not available right now.This is an automated message from SPARKZZZ-BOT inbox security.kindly wait till my master approves  you..or tag him in group\n\n"
-                    f"{MESAG}"
-                    "\n\n\n - Thank You 🙏")
+                    "**Send** `/start` **And Register A Request**\n\n"
+                    f"**{MESAG}**"
+                    )
 
 
 if Var.PRIVATE_GROUP_ID is not None:
