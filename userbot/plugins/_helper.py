3e6445f7bd6b233a9ab71ee06b321c7d9de42df1
VISHNU CS <mailvishnucs@gmail.com>
2020-10-05 19:07:10 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -44,7 +44,7 @@ async def cmd_list(event):
             else:
                 await event.reply(input_str + " is not a valid plugin!")
         else:
-            help_string = f"SPARKZZZ module Helper.. Provided by {DEFAULTUSER}\
+            help_string = f"Userbot Helper.. Provided by {DEFAULTUSER}\
                           \n`Userbot Helper for to reveal all the commands of `**[SPARKZZZ](https://github.com/vishnu175/SPARKZZZ/)**\
                           \nSpecify which module do you want help for!\
                           \n\n__Type__ `.help` __plugin_name to know how the modules work.__"
