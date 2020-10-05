cd6b9b1dce5ed6ca3049c1ae23a520f6ae06a15c
VISHNU CS <mailvishnucs@gmail.com>
2020-10-05 13:22:11 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,26 +1,21 @@
-# Ported from other Telegram UserBots for TeleBot
-# Kangers, don't remove this line 
-# @csv1990
-
 from userbot import CMD_LIST
 from userbot import ALIVE_NAME
 from userbot.utils import admin_cmd
 from platform import uname
-from telethon import TelegramClient
 import sys
 from telethon import events, functions, __version__
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SPARKZZZ User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SPARKZZZ user"
 
-@telebot.on(admin_cmd(pattern="help ?(.*)"))
+@sparkzzz.on(admin_cmd(pattern="help ?(.*)"))
+@sparkzzz.on(admin_cmd(pattern="help ?(.*)", allow_sudo=True))
 async def cmd_list(event):
-    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
         tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
         input_str = event.pattern_match.group(1)
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "⚡ " + i + "\n"
+                string += "⚡" + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
@@ -38,27 +33,67 @@ async def cmd_list(event):
                     )
                     await event.delete()
             else:
-                await event.edit(string)
+                await event.reply(string)
         elif input_str:
             if input_str in CMD_LIST:
-                string = "**Commands available in {}** \n\n".format(input_str)
+                string = "Commands found in {}:".format(input_str)
                 for i in CMD_LIST[input_str]:
                     string += "    " + i
                     string += "\n"
-                string +="\n**© @sparkzzzbotsupport**"
-                await event.edit(string)
+                await event.reply(string)
             else:
-                await event.edit(input_str + " is not a valid plugin!")
+                await event.reply(input_str + " is not a valid plugin!")
         else:
             help_string = f"""Userbot Helper.. Provided by ⚡{DEFAULTUSER} \n
-`SPARKZZZ Helper to reveal all the commands`\n__Do .help plugin_name to know how its works.__"""
+`SPARKZZZ Helper to reveal all the commands`\n__Do .help plugin_name for commands, in case popup doesn't appear.__"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
-            )          
+            )
             await results[0].click(
                 event.chat_id,
                 reply_to=event.reply_to_msg_id,
                 hide_via=True
             )
             await event.delete()
+            
+@sparkzzz.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
+@sparkzzz.on(admin_cmd(pattern="dc", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetNearestDcRequest())  # pylint:disable=E0602
+    await event.reply(result.stringify())
+
+
+@sparkzzz.on(admin_cmd(pattern="config"))  # pylint:disable=E0602
+@sparkzzz.on(admin_cmd(pattern="config", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    result = await borg(functions.help.GetConfigRequest())  # pylint:disable=E0602
+    result = result.stringify()
+    logger.info(result)  # pylint:disable=E0602
+    await event.reply("""Telethon UserBot powered by JARVIS UserBot""")
+
+
+@sparkzzz.on(admin_cmd(pattern="syntax (.*)"))
+@sparkzzz.on(admin_cmd(pattern="syntax (.*)", allow_sudo=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    plugin_name = event.pattern_match.group(1)
+
+    if plugin_name in CMD_LIST:
+        help_string = CMD_LIST[plugin_name].__doc__
+        unload_string = f"Use `.unload {plugin_name}` to remove this plugin.\n           © SPARKZZZ UserBot"
+        
+        if help_string:
+            plugin_syntax = f"Syntax for plugin **{plugin_name}**:\n\n{help_string}\n{unload_string}"
+        else:
+            plugin_syntax = f"No DOCSTRING has been setup for {plugin_name} plugin."
+    else:
+
+        plugin_syntax = "Enter valid **Plugin** name.\nDo `.plinfo` or `.help` to get list of valid plugin names."
+
+    await event.reply(plugin_syntax)
