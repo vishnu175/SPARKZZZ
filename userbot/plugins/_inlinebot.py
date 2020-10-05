20db3d54695c9ae57acf0e60942ea0fb0034d29a
VISHNU CS <mailvishnucs@gmail.com>
2020-10-05 23:39:17 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -5,7 +5,7 @@ from telethon import Button, custom, events
 from userbot import CMD_LIST
 from . import telealive
 
-ALIVE_PIC = Config.ALIVE_PIC if Config.ALIVE_PIC else None
+SPARKZZZ_IMG = Config.ALIVE_PIC if Config.ALIVE_PIC else None
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
 
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
