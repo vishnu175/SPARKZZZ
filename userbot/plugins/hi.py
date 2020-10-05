73124a73cda56c36daf302e6d33d0aef687cfba8
VISHNU CS <mailvishnucs@gmail.com>
2020-10-05 11:53:14 +0530

    Create hi.py

---
+++ userbot/plugins/hi.py
@@ -0,0 +1,15 @@
+from userbot.utils import admin_cmd
+from userbot import events
+
+@sparkzzz.on(admin_cmd(pattern=r"hhi ?(.*)"))
+async def hhi(event):
+    giveVar = event.text
+    cat = giveVar[5:6]
+    if not cat:
+        cat = "🌀"
+    ct = giveVar[7:8]
+    if not ct:
+        ct = "⚡"
+    await event.edit(
+        f"{cat}{ct}{ct}{cat}{ct}{cat}{cat}{cat}\n{cat}{ct}{ct}{cat}{ct}{ct}{cat}{ct}\n{cat}{cat}{cat}{cat}{ct}{ct}{cat}{ct}\n{cat}{ct}{ct}{cat}{ct}{ct}{cat}{ct}\n{cat}{ct}{ct}{cat}{ct}{cat}{cat}{cat}\n☁☁☁☁☁☁☁☁"
+    )
