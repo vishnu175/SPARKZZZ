d359c4f34a7bcc5acc62b6cce068c11e570d3d62
VISHNU CS <mailvishnucs@gmail.com>
2020-10-06 12:51:38 +0530

    Update welcome_sql.py

--- userbot/plugins/sql_helper/welcome_sql.py
+++ userbot/plugins/sql_helper/welcome_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import BigInteger, Boolean, Column, String, UnicodeText
-from jarvis.plugins.sql_helper import SESSION, BASE
+from userbot.plugins.sql_helper import SESSION, BASE
 
 
 class Welcome(BASE):
