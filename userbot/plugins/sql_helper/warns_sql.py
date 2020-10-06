d4a7f8979325280e044aa5ee89f8f33382360524
VISHNU CS <mailvishnucs@gmail.com>
2020-10-06 12:50:58 +0530

    Update warns_sql.py

--- userbot/plugins/sql_helper/warns_sql.py
+++ userbot/plugins/sql_helper/warns_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Integer, Column, String, UnicodeText, func, distinct, Boolean
 
-from jarvis.plugins.sql_helper import SESSION, BASE
+from userbot.plugins.sql_helper import SESSION, BASE
 
 
 class Warns(BASE):
