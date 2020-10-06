b530f1befc17acb909fb78b7b0643ab5334bb10a
VISHNU CS <mailvishnucs@gmail.com>
2020-10-07 02:11:47 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -1,15 +1,15 @@
+#(C) SPARKZZZ 2020
+# @vishnu175
 import requests
 from telethon import functions
-
 from userbot import ALIVE_NAME
-
 from .. import CMD_HELP, CMD_LIST
 from ..utils import admin_cmd, edit_or_reply, sudo_cmd
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Sparkzzz user"
 
 
-@borg.on(admin_cmd(pattern="help ?(.*)"))
+@sparkzzz.on(admin_cmd(pattern="help ?(.*)"))
 async def cmd_list(event):
     input_str = event.pattern_match.group(1)
     tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
@@ -32,7 +32,7 @@ async def cmd_list(event):
                 .get("key")
             )
             url = f"https://nekobin.com/{key}"
-            reply_text = f"All commands of the catuserbot are [here]({url})"
+            reply_text = f"All commands of the sparkzzzuserbot are [here]({url})"
             await event.edit(reply_text)
             return
         await event.edit(string)
@@ -50,8 +50,9 @@ async def cmd_list(event):
         else:
             help_string = f"Userbot Helper.. Provided by {DEFAULTUSER}\
                           \n`All modules of `**[SPARKZZZ]**(https://github.com/vishnu175/SPARKZZZ/) are listed here\
-                          \n__**Type__ `.help`<module name>** to know usage of modules."
-                results = await bot.inline_query(  # pylint:disable=E0602
+                          \n__**Type__ `.help`<module name>** to know usage of modules.\
+                          \nDo `.info` plugin_name for usage"
+            results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
             )
             await results[0].click(
@@ -69,13 +70,15 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            string = f"**Userbot Helper.. Provided by [{DEFAULTUSER}]\nUserbot Helper to reveal all the plugin names\n\n**Do `.help` plugin_name for commands\nDo `.info` plugin_name for usage\n\n"
+            string = f"**Userbot Helper.. Provided by [{DEFAULTUSER}]\nUserbot Helper to reveal all the plugin names\n\n**Do `.help` for commands\nDo `.help` plugin_name for usage\n\n"
             for i in sorted(CMD_LIST):
                 string += "◆`" + str(i)
                 string += "`   "
             await event.edit(string)
 
-@borg.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
+
+
+@sparkzzz.on(admin_cmd(pattern="dc"))  # pylint:disable=E0602
 async def _(event):
     if event.fwd_from:
         return
