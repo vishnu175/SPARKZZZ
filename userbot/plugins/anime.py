ead73fc783cb8ea464c82dbbea88ed4a50137b0e
VISHNU CS <mailvishnucs@gmail.com>
2020-10-06 23:30:48 +0530

    Update anime.py

--- userbot/plugins/anime.py
+++ userbot/plugins/anime.py
@@ -2,7 +2,7 @@ from telethon import events
 import asyncio
 import os
 import sys
-from uniborg.util import admin_cmd
+from userbot.utils import admin_cmd
 
 @borg.on(admin_cmd(pattern=r"lmoon"))
 async def test(event):
