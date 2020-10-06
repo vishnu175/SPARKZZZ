0e3f4cde11aa687059be6c7622ead32651ac987f
VISHNU CS <mailvishnucs@gmail.com>
2020-10-06 12:42:02 +0530

    Update antiflood_sql.py

--- userbot/plugins/sql_helper/antiflood_sql.py
+++ userbot/plugins/sql_helper/antiflood_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, Integer, String
 
-from jarvis.plugins.sql_helper import BASE, SESSION
+from userbot.plugins.sql_helper import BASE, SESSION
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
