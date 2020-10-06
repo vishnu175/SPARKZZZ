e4604aad62effcbadf7087b976923589bcfaeb31
VISHNU CS <mailvishnucs@gmail.com>
2020-10-06 12:44:29 +0530

    Update filter_sql.py

--- userbot/plugins/sql_helper/filter_sql.py
+++ userbot/plugins/sql_helper/filter_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, UnicodeText, LargeBinary, Numeric, String
-from jarvis.plugins.sql_helper import SESSION, BASE
+from userbot.plugins.sql_helper import SESSION, BASE
 
 
 class Filters(BASE):
