c063eda1c833f7fd8c7bfef1c25d7b45bfd478d4
VISHNU CS <mailvishnucs@gmail.com>
2020-10-06 12:45:21 +0530

    Update gban_sql_helper.py

--- userbot/plugins/sql_helper/gban_sql_helper.py
+++ userbot/plugins/sql_helper/gban_sql_helper.py
@@ -1,5 +1,5 @@
 try:
-    from jarvis.plugins.sql_helper import SESSION, BASE
+    from userbot.plugins.sql_helper import SESSION, BASE
 except ImportError:
     raise AttributeError
 from sqlalchemy import Column, String, UnicodeText, Boolean, Integer, distinct, func
