ff4a483fff3ae80ce7196f38ce58045e7633328f
VISHNU CS <mailvishnucs@gmail.com>
2020-10-07 16:29:31 +0530

    Update biblewords.py

--- userbot/plugins/biblewords.py
+++ userbot/plugins/biblewords.py
@@ -35,29 +35,29 @@ async def _(event):
     if (x==8):
       await event.edit("**“Blessed are those who are persecuted because of righteousness, for theirs is the kingdom of heaven.” - Mathew 5:10**")
     if (x==9):
-      await event.edit("**“Jesus answered, “Everyone who drinks this water will be thirsty again, but whoever drinks the water I give them will never thirst. Indeed, the water I give them will become in them a spring of water welling up to eternal life.” - John 4:13-14**")
+      await event.edit("**“Jesus answered, Everyone who drinks this water will be thirsty again, but whoever drinks the water I give them will never thirst. Indeed, the water I give them will become in them a spring of water welling up to eternal life.” - John 4:13-14**")
     if (x==10):
-      await event.edit("**“Jesus answered them, “It is not the healthy who need a doctor, but the sick.I have not come to call the righteous, but sinners to repentance.” - Luke 5:31-32**")
+      await event.edit("**“Jesus answered them, It is not the healthy who need a doctor, but the sick.I have not come to call the righteous, but sinners to repentance.” - Luke 5:31-32**")
     if (x==11):
       await event.edit("**“So in everything, do to others what you would have them do to you, for this sums up the Law and the Prophets.” - Mathew 7:12**")
     if (x==12):
-      await event.edit("**"I have told you these things, so that in me you may have peace. In this world you will have trouble. But take heart! I have overcome the world." - John 16:33**")
+      await event.edit("**“I have told you these things, so that in me you may have peace, in this world you will have trouble,But take heart I have overcome the world." - John 16:33**")
     if (x==13):
-      await event.edit("**"But he said to me, My grace is sufficient for you, for my power is made perfect in weakness. Therefore I will boast all the more gladly about my weaknesses, so that Christ’s power may rest on me." - 2 Corinthians 12:9**")
+      await event.edit("**”But he said to me, My grace is sufficient for you, for my power is made perfect in weakness. Therefore I will boast all the more gladly about my weaknesses, so that Christ’s power may rest on me." - 2 Corinthians 12:9**")
     if (x==14):
-      await event.edit("**"Jesus looked at them and said, With man this is impossible, but with God all things are possible." - Mathew 19:26**")
+      await event.edit("**“Jesus looked at them and said, With man this is impossible, but with God all things are possible.“ - Mathew 19:26**")
     if (x==15):
-      await event.edit("**"You have heard that it was said,Love your neighbor and hate your enemy.  But I tell you, love your enemies and pray for those who persecute you," - Mathew 5:43-44**")
+      await event.edit("**“You have heard that it was said,Love your neighbor and hate your enemy.  But I tell you, love your enemies and pray for those who persecute you," - Mathew 5:43-44**")
     if (x==16):
-      await event.edit("**"So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand." - Isaiah 41:10**")
+      await event.edit("**“So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand." - Isaiah 41:10**")
     if (x==17):
-      await event.edit("**"My flesh and my heart may fail, but God is the strength of my heart and my portion forever." - Psalm  73:26**")
+      await event.edit("**“My flesh and my heart may fail, but God is the strength of my heart and my portion forever.“ - Psalm  73:26**")
     if (x==18):
-      await event.edit("**" I pray that out of his glorious riches he may strengthen you with power through his Spirit in your inner being." - Ephesians 3:16**")
+      await event.edit("**“I pray that out of his glorious riches he may strengthen you with power through his Spirit in your inner being." - Ephesians 3:16**")
     if (x==19):
-      await event.edit("**"LORD my God, I called to you for help, and you healed me." - Psalms 30:2**")
+      await event.edit("**“LORD my God, I called to you for help, and you healed me.“ - Psalms 30:2**")
     if (x==20):
-      await event.edit("**"I said, Have mercy on me, LORD; heal me, for I have sinned against you." - Psalms 41:4**")	
+      await event.edit("**“I said, Have mercy on me, LORD; heal me, for I have sinned against you.“ - Psalms 41:4**")	
       
       
       
