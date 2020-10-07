8520b6e69143b4099aa78e0f57ab511ea8e719b2
VISHNU CS <mailvishnucs@gmail.com>
2020-10-07 15:59:36 +0530

    Add files via upload

---
+++ userbot/plugins/biblewords.py
@@ -0,0 +1,65 @@
+# (C) SPARKZZZ 2020
+# created by vishnu175
+# Kangers please keep credit
+"""
+Gives you the WORD OF GOD...
+Syntax: .jesus
+creator : @vishnu175"""
+from telethon import events
+import asyncio
+import os
+import sys
+import random
+
+
+
+@sparkzzz.on(events.NewMessage(pattern=r"\.jesus", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    x=(random.randrange(1,20)) 
+    if (x==1):
+      await event.edit("**“Jesus answered, I am the way and the truth and the life. No one comes to the Father except through me.” - john 14:6**")
+    if (x==2):
+      await event.edit("**“But seek first his kingdom and his righteousness, and all these things will be given to you as well” - Mathew 6:33**")
+    if (x==3):
+      await event.edit("**“In the same way, let your light shine before others, that they may see your good deeds and glorify your Father in heaven” - Mathew 5:16**")
+    if (x==4):
+      await event.edit("**“Ask and it will be given to you; seek and you will find; knock and the door will be opened to you. For everyone who asks receives; the one who seeks finds; and to the one who knocks, the door will be opened.” - Mathew 7:7-8**")
+    if (x==5):
+      await event.edit("**“Father, forgive them, for they know not what they do.” - Luke 23:34**")
+    if (x==6):
+      await event.edit("**“For those who exalt themselves will be humbled, and those who humble themselves will be exalted.” - Mathew 23:12**")
+    if (x==7):
+      await event.edit("**“Therefore I tell you, whatever you ask for in prayer, believe that you have received it, and it will be yours. And when you stand praying, if you hold anything against anyone, forgive them, so that your Father in heaven may forgive you your sins.” - Mark 11:24-25**")
+    if (x==8):
+      await event.edit("**“Blessed are those who are persecuted because of righteousness, for theirs is the kingdom of heaven.” - Mathew 5:10**")
+    if (x==9):
+      await event.edit("**“Jesus answered, “Everyone who drinks this water will be thirsty again, but whoever drinks the water I give them will never thirst. Indeed, the water I give them will become in them a spring of water welling up to eternal life.” - John 4:13-14**")
+    if (x==10):
+      await event.edit("**“Jesus answered them, “It is not the healthy who need a doctor, but the sick.I have not come to call the righteous, but sinners to repentance.” - Luke 5:31-32**")
+    if (x==11):
+      await event.edit("**“So in everything, do to others what you would have them do to you, for this sums up the Law and the Prophets.” - Mathew 7:12**")
+    if (x==12):
+      await event.edit("**"I have told you these things, so that in me you may have peace. In this world you will have trouble. But take heart! I have overcome the world." - John 16:33**")
+    if (x==13):
+      await event.edit("**"But he said to me, My grace is sufficient for you, for my power is made perfect in weakness. Therefore I will boast all the more gladly about my weaknesses, so that Christ’s power may rest on me." - 2 Corinthians 12:9**")
+    if (x==14):
+      await event.edit("**"Jesus looked at them and said, With man this is impossible, but with God all things are possible." - Mathew 19:26**")
+    if (x==15):
+      await event.edit("**"You have heard that it was said,Love your neighbor and hate your enemy.  But I tell you, love your enemies and pray for those who persecute you," - Mathew 5:43-44**")
+    if (x==16):
+      await event.edit("**"So do not fear, for I am with you; do not be dismayed, for I am your God. I will strengthen you and help you; I will uphold you with my righteous right hand." - Isaiah 41:10**")
+    if (x==17):
+      await event.edit("**"My flesh and my heart may fail, but God is the strength of my heart and my portion forever." - Psalm  73:26**")
+    if (x==18):
+      await event.edit("**" I pray that out of his glorious riches he may strengthen you with power through his Spirit in your inner being." - Ephesians 3:16**")
+    if (x==19):
+      await event.edit("**"LORD my God, I called to you for help, and you healed me." - Psalms 30:2**")
+    if (x==20):
+      await event.edit("**"I said, Have mercy on me, LORD; heal me, for I have sinned against you." - Psalms 41:4**")	
+      
+      
+      
+  # WORD OF GOD is vast...i have added only a few, more WORDS will be added soon    
+	
