e3d0f549f21c551f6ceee4f3166466f5393c5adc
VISHNU CS <mailvishnucs@gmail.com>
2020-10-08 23:06:25 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -42,6 +42,8 @@ if ENV:
         # DO NOT EDIT BELOW THIS LINE IF YOU DO NOT KNOW WHAT YOU ARE DOING
         # TG API limit. A message can have maximum 4096 characters!
         MAX_MESSAGE_SIZE_LIMIT = 4095
+         # set this to your fork on GitHub (if you want)
+        OFFICIAL_UPSTREAM_REPO = os.environ.get("OFFICIAL_UPSTREAM_REPO", "https://github.com/vishnu175/SPARKZZZ")
         # set blacklist_chats where you do not want userbot's features
         UB_BLACK_LIST_CHAT = set(int(x) for x in os.environ.get("UB_BLACK_LIST_CHAT", "").split())
         # maximum number of messages for antiflood
