76de5e637570f38a702e218d55510cbf02eb8639
VISHNU CS <mailvishnucs@gmail.com>
2020-10-09 22:26:17 +0530

    Update install_ext_module.py

--- userbot/plugins/install_ext_module.py
+++ userbot/plugins/install_ext_module.py
@@ -3,14 +3,12 @@ from telethon import events
 from telethon import functions, types
 from telethon.tl.types import InputMessagesFilterDocument
 from userbot.utils import command, remove_plugin, load_module
-from var import Var
 from pathlib import Path
-from userbot import LOAD_PLUG
+from userbot utils import admin_cmd
 import sys
 import asyncio
 import traceback
 import os
-import userbot.utils
 
 @sparkzzz.on(admin_cmd(pattern=r"extdl$"))
 async def install(event):
