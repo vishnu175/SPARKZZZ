4bc4d9fb50ab08ecde4534696a57ee9018b9f3d9
VISHNU CS <mailvishnucs@gmail.com>
2020-10-09 22:08:47 +0530

    Update install_ext_module.py

--- userbot/plugins/install_ext_module.py
+++ userbot/plugins/install_ext_module.py
@@ -14,31 +14,31 @@ import userbot.utils
 
 @command(pattern="^.extdl", outgoing=True)
 async def install(event):
-    if event.fwd_from:
-        return
-    documentss = await event.client.get_messages(event.chat_id, None ,search='.py', filter=InputMessagesFilterDocument)
-    total = int(documentss.total)
-    total_doxx = range(0, total)
-    b = await event.client.send_message(event.chat_id, f"**Installing {total} plugins...**\n`This msg will be deleted after the installation gets completed`")
-    text =  "**Installing Plugins...**\n\n"
-    a = await event.client.send_message(event.chat_id, text)
-    if total == 0:
-	    await a.edit("**No plugins to install.**")
-	    await event.delete()
-	    return
-    for ixo in total_doxx:
-        mxo = documentss[ixo].id
-        downloaded_file_name = await event.client.download_media(await borg.get_messages(chat, ids=mxo), "userbot/plugins/")
-        if "(" not in downloaded_file_name:
-            path1 = Path(downloaded_file_name)
-            shortname = path1.stem
-            try:
-		     load_module(shortname.replace(".py", ""))
-		     text += f"**• Installed** `{(os.path.basename(downloaded_file_name))}` **successfully.**\n"
-	    except:
-		     text += f"**• Error installing** `{(os.path.basename(downloaded_file_name))}`\n"
-	    else:
+        if event.fwd_from:
+                return
+        documentss = await event.client.get_messages(event.chat_id, None ,search='.py', filter=InputMessagesFilterDocument)
+	total = int(documentss.total)
+	total_doxx = range(0, total)
+	b = await event.client.send_message(event.chat_id, f"**Installing {total} plugins...**\n`This msg will be deleted after the installation gets completed`")
+	text =  "**Installing Plugins...**\n\n"
+	a = await event.client.send_message(event.chat_id, text)
+	if total == 0:
+		await a.edit("**No plugins to install.**")
+		await event.delete()
+		return
+	for ixo in total_doxx:
+		mxo = documentss[ixo].id
+		downloaded_file_name = await event.client.download_media(await event.client.get_messages(event.chat_id, ids=mxo), "userbot/plugins/")
+		if "(" not in downloaded_file_name:
+			path1 = Path(downloaded_file_name)
+			shortname = path1.stem
+			try:
+				load_module(shortname.replace(".py", ""))
+				text += f"**• Installed** `{(os.path.basename(downloaded_file_name))}` **successfully.**\n"
+			except:
+				text += f"**• Error installing** `{(os.path.basename(downloaded_file_name))}`\n"
+		else:
 			text += f"**• Plugin** `{(os.path.basename(downloaded_file_name))}` **already installed.**\n"
-	    await a.edit(f"{text}\n**Installed every plugin.**")
-	    await event.delete()
-	    await b.delete()
+		await a.edit(f"{text}\n**Installed every plugin.**")
+		await event.delete()
+		await b.delete()
