55cc4947479a8aad5244da8692de3dda25f4ad2f
VISHNU CS <mailvishnucs@gmail.com>
2020-10-11 04:06:06 +0530

    Update uniborgConfig.py

--- userbot/uniborgConfig.py
+++ userbot/uniborgConfig.py
@@ -136,6 +136,7 @@ if ENV:
         PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
         CUSTOM_PMPERMIT_TEXT = os.environ.get("CUSTOM_PMPERMIT_TEXT", None)
         PM_DATA = os.environ.get("PM_DATA", "ENABLE")
+        NEWS_CHANNEL_ID = int(os.environ.get("NEWS_CHANNEL_ID", False))
         
         
 else:
