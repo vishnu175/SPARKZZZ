a457ae70f4342214406c658495da9f38f0586dec
VISHNU CS <mailvishnucs@gmail.com>
2020-10-11 01:10:57 +0530

    Update updater.py

--- userbot/plugins/updater.py
+++ userbot/plugins/updater.py
@@ -108,7 +108,6 @@ async def updater(message):
                     remote.set_url(heroku_git_url)
                 else:
                     remote = repo.create_remote("heroku", heroku_git_url)
-                try:
                     remote.push("refspec=refspec", force=True)
                 asyncio.get_event_loop().create_task(
                     deploy_start(tgbot, message,)
