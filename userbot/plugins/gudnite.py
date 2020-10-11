368f28290a3eac857f5b43376f6bc3dd3125f5d3
VISHNU CS <mailvishnucs@gmail.com>
2020-10-11 12:14:12 +0530

    Update gudnite.py

--- userbot/plugins/gudnite.py
+++ userbot/plugins/gudnite.py
@@ -6,24 +6,12 @@
 
 """
 
-from telethon.tl.functions.channels import LeaveChannelRequest
-
 from userbot.utils import admin_cmd
 
 import time
 
 @sparkzzz.on(admin_cmd(pattern="gdn", outgoing=True))
 
-async def leave(e):
 
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-
-        await e.edit("`GOOD NIGHT SWEET DREAMS 😘😘😘.`")
-
-        time.sleep(3)
-
-        if '-' in str(e.chat_id):
-
-            await sparkzzz(LeaveChannelRequest(e.chat_id))
 
         
