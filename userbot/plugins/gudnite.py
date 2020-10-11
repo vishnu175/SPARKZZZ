74dc35d15eac92958679afb037520f57ebba5ffd
VISHNU CS <mailvishnucs@gmail.com>
2020-10-11 12:42:10 +0530

    Update gudnite.py

--- userbot/plugins/gudnite.py
+++ userbot/plugins/gudnite.py
@@ -1,29 +1,32 @@
-# (C) SPARKZZZ 2020
+# @ (C) SPARKZZZZ 2020
 
 """
-
 .gdn
-
 """
 
-from telethon.tl.functions.channels import LeaveChannelRequest
+from telethon import events
+
+import asyncio
 
 from userbot.utils import admin_cmd
 
-import time
+@sparkzzz.on(admin_cmd(pattern="(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(0, 4)
+    input_str = event.pattern_match.group(1)
+    if input_str == "gdn":
+        await event.edit(input_str)
+        animation_chars = [
+            "Gud",
+            "Gud night",
+            "Gud night sweet",
+            "Gud night sweet dreams 😴😴",
+        ]
 
-@sparkzzz.on(admin_cmd(pattern="gdn", outgoing=True))
-
-async def leave(e):
-
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-
-        await e.edit("`GOOD NIGHT SWEET DREAMS 😘😘😘.`")
-
-        time.sleep(3)
-
-        if '-' in str(e.chat_id):
-
-            await sparkzzz(LeaveChannelRequest(e.chat_id))
-
-        
+        for i in animation_ttl:
+        	
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i %4 ])
