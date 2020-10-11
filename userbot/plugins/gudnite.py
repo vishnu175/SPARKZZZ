a44e8f1023ee31b57132b93ee1e0d9cca7612f50
VISHNU CS <mailvishnucs@gmail.com>
2020-10-11 12:18:31 +0530

    Update gudnite.py

--- userbot/plugins/gudnite.py
+++ userbot/plugins/gudnite.py
@@ -6,12 +6,29 @@
 
 """
 
+from telethon import events
+
+import asyncio
+
 from userbot.utils import admin_cmd
 
-import time
+@borg.on(admin_cmd(pattern="(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    animation_interval = 0.3
+    animation_ttl = range(0, 4)
+    input_str = event.pattern_match.group(1)
+    if input_str == "gdn":
+        await event.edit(input_str)
+        animation_chars = [
+            "Gud",
+            "Gud night",
+            "Gud night sweet",
+            "Gud night sweet dreams",
+        ]
 
-@sparkzzz.on(admin_cmd(pattern="gdn", outgoing=True))
-
-
-
-        
+        for i in animation_ttl:
+        	
+            await asyncio.sleep(animation_interval)
+            await event.edit(animation_chars[i %4 ])
