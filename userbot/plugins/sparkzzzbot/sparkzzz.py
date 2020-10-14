381520c12f3b188122b249b3a153d79751663a67
VISHNU CS <mailvishnucs@gmail.com>
2020-10-14 16:28:18 +0530

    Update sparkzzz.py

--- userbot/plugins/sparkzzzbot/sparkzzz.py
+++ userbot/plugins/sparkzzzbot/sparkzzz.py
@@ -11,3 +11,33 @@ from userbot import bot
 from userbot import ALIVE_NAME
 from userbot.uniborgConfig import Config
 from heroku_config import Var
+
+
+
+DEFUSR = str(ALIVE_NAME) if ALIVE_NAME else "SPARKZZZ-user'
+
+@tgbot.on(events.NewMessage(pattern="^/start"))
+async def start(event):
+    await tgbot.send_message(
+           event.chat_id,
+           message=startinfo,
+           buttons = [
+           [Button.url("SPARKZZZ-REPO", "https://github.com/vishnu175/SPARKZZZ/")]
+           [Button.url("SPARKZZZ", "t.me/sparkzzzbothelp")]
+            ]
+      )
+    
+    
+tgbot.on(events.NewMessage(pattern="^/help"))
+async def thisfn(event):
+    await tgbot.send_message(
+           event.chat_id,
+           message=helpinfo,
+           link_preview = False,
+           buttons = [
+           [Button.url("SPARKZZZ", "https://t.me/sparkzzzbothelp")]
+            ]
+      )  
+    
+    
+    
