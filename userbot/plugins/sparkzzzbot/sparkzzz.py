af82e584d443e88008873f9f0d243883469bdd10
VISHNU CS <mailvishnucs@gmail.com>
2020-10-14 15:12:58 +0530

    Update sparkzzz.py

--- userbot/plugins/sparkzzzbot/sparkzzz.py
+++ userbot/plugins/sparkzzzbot/sparkzzz.py
@@ -2,3 +2,12 @@ from telethon import events, custom, Button
 from telethon import events
 from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 import time
+import os
+import requests
+import asyncio
+import emoji
+from datetime import datetimemport math
+from userbot import bot
+from userbot import ALIVE_NAME
+from userbot.uniborgConfig import Config
+from heroku_config import Var
