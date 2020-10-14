74426dee7816af824baa0daead98aca7755bfe4b
VISHNU CS <mailvishnucs@gmail.com>
2020-10-15 02:21:06 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -34,7 +34,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 buttons=buttons,
                 link_preview=False
             )
-        elif event.query.user_id == bot.uid and query == "stats":
+        if event.query.user_id == bot.uid and query == "stats":
             result = builder.article(
                 title="Stats",
                 text=f"**SPARKZZZ Stats For [{DEFAULTUSER}](tg://user?id={myid})**\n\n__Bot is functioning normally, master!__\n\n(c) @sparkzzzbothelp",
