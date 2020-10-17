6f64fd812fd10215e4c3c7a2e41551fc1dc82872
VISHNU CS <mailvishnucs@gmail.com>
2020-10-17 23:46:53 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -181,6 +181,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
     @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"enquirestat")))
     async def rip(event):
+        if event.query.user_id == bot.uid:
         text = sparkzzzstatus
         await event.answer(text, alert=True)
 
