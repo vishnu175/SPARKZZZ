80711c4c0ec9a869b547ee6776d6d24939911523
VISHNU CS <mailvishnucs@gmail.com>
2020-10-17 22:39:06 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -9,7 +9,7 @@ from userbot.utils import admin_cmd
 from telethon.tl.functions.users import GetFullUserRequest
 from heroku_config import Config
 
-SPARKZZZ_IMG = Config.ALIVE_PIC if Config.ALIVE_PIC else None
+
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
 
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
