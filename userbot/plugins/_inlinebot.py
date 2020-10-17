7421280c2273e04393ba12dfbbcf183dd64f0810
VISHNU CS <mailvishnucs@gmail.com>
2020-10-17 20:36:12 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -11,34 +11,34 @@ BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>
 
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
-    @tgbot.on(events.InlineQuery)
+    @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
     async def inline_handler(event):
         builder = event.builder
         result = None
         query = event.text
-        if query.startswith("**SPARKZZZ") and event.query.user_id == bot.uid:
-            buttons = [
-                (
-                    custom.Button.inline("Stats", data="stats"),
-                    Button.url("Repo", "https://github.com/vishnu175/SPARKZZZ"),
-                )
-            ]
-            result = builder.article(
-                # sparkzzzpic,
-                title="Alive sparkzzz",
-                # force_document = False,
-                text=query,
-                buttons=buttons,
-            )
-            await event.answer([result] if result else None)
-        elif event.query.user_id == bot.uid and query.startswith("SPARKZZZ"):
+        if event.query.user_id == bot.uid and query.startswith("SPARKZZZ"):
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "© sparkzzzbothelp",
-                text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),
+                "© SPARKZZZ Help",
+                text="{}\nCurrently Loaded Plugins: {}".format(
+                    query, len(CMD_LIST)),
                 buttons=buttons,
                 link_preview=False,
+            )
+        if event.query.user_id == bot.uid and query == "stats":
+            result = builder.article(
+                title="Stats",
+                text=f"**SPARKZZZ Stats For {DEFAULTUSER}'s(tg://user?id={myid})** \n⚡SPARKZZZ INSIDE⚡\n(C) @[SPARKZZZ](t.me/sparkzzzbothelp)",
+                buttons=[
+                    [custom.Button.inline("Stats", data="terminator")],
+                    [
+                        Button.url(
+                            "Repo",
+                            "https://github.com/vishnu175/SPARKZZZ")
+                    ],
+                    [Button.url("Join Channel ❤️", "t.me/Fridayot")],
+                ],
             )
             await event.answer([result] if result else None)
         elif event.query.user_id == bot.uid and query.startswith("Inline buttons"):
@@ -177,12 +177,10 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
-    async def on_plug_in_callback_query_handler(event):
-        statstext = await catalive()
-        reply_pop_up_alert = statstext
-        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"enquirestat")))
+    async def rip(event):
+        text = sparkzzzstatus
+        await event.answer(text, alert=True)
 
 def paginate_help(page_number, loaded_plugins, prefix):
     number_of_rows = Config.NO_OF_INLINE_ROWS
