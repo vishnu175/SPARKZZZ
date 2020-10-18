856b4f1ea6ce74a225ab71d08231eb0730460c06
VISHNU CS <mailvishnucs@gmail.com>
2020-10-18 10:31:39 +0530

    Create muzik.py

---
+++ userbot/plugins/muzik.py
@@ -0,0 +1,20 @@
+
+@sparkzzz.on(admin_cmd(outgoing=True, pattern="muz(?: |$)(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    link = event.pattern_match.group(1)
+    chat = "@MuzzzBot"
+    await event.edit("```Getting Your Music```")
+    async with bot.conversation(chat) as conv:
+          await asyncio.sleep(2)
+          await event.edit("`Downloading music taking some times,  Stay Tuned.....`")
+          try:
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=752979930))
+              await bot.send_message(chat, link)
+              respond = await response
+          except YouBlockedUserError:
+              await event.reply("```Please unblock @MuzzzBot and try again```")
+              return
+          await event.delete()
+          await bot.forward_messages(event.chat_id, respond.message)
