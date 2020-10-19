2b107b6dbba75a696602be55a198b7f343d44ad8
VISHNU CS <mailvishnucs@gmail.com>
2020-10-19 12:28:36 +0530

    Create chrome.py

---
+++ userbot/chrome.py
@@ -0,0 +1,30 @@
+# (C) SPARKZZZ 2020
+
+import os
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+from userbot.import TEMP_DOWNLOAD_DIRECTORY, GOOGLE_CHROME_BIN, CHROME_DRIVER
+
+
+async def chrome(chrome_options=None):
+    if chrome_options is None:
+        chrome_options = await options()
+    if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
+        os.mkdir(TEMP_DOWNLOAD_DIRECTORY)
+    prefs = {'download.default_directory': TEMP_DOWNLOAD_DIRECTORY}
+    chrome_options.add_experimental_option('prefs', prefs)
+    return webdriver.Chrome(executable_path=CHROME_DRIVER,
+                            options=chrome_options)
+
+
+async def options():
+    chrome_options = Options()
+    chrome_options.binary_location = "/app/.apt/usr/bin/google-chrome"
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--window-size=1920x1080")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
+    return chrome_options
