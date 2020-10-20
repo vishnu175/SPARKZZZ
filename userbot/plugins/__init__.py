25feac16cb57ba27d83a217804c140103827d4b5
VISHNU CS <mailvishnucs@gmail.com>
2020-10-20 09:04:29 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,51 +1,90 @@
-import html
-import time
-from datetime import datetime
-
-from telethon.tl.functions.photos import GetUserPhotosRequest
-from telethon.tl.functions.users import GetFullUserRequest
-from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-
-from heroku_config import Var
-from userbot import ALIVE_NAME, sparkzzzver
+from datetime import datetime
+import asyncio
+import time
+import os
+import re
+import time
+import math
+import heroku3
+import requests
+import html
+from telethon.tl.types import MessageEntityMentionName
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.functions.photos import GetUserPhotosRequest
 from userbot.__init__ import StartTime
+from userbot import ALIVE_NAME
+from heroku_config import Var
+from userbot.utils import admin_cmd
+from userbot.utils import  sudo_cmd
+from userbot import sparkzzzver
+from telethon import events
 from userbot.uniborgConfig import Config
-from userbot.utils import admin_cmd, sudo_cmd
 
-# stats
-if Config.PRIVATE_GROUP_BOT_API_ID:
-    log = "Enabled"
+SPARKZZZUSER = str(ALIVE_NAME) if ALIVE_NAME else "SPARKZZZ user"
+
+Heroku = heroku3.from_key(Var.HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+HEROKU_APP_NAME = Var.HEROKU_APP_NAME
+HEROKU_API_KEY = Var.HEROKU_API_KEY
+
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>SPARKZZZ-BOT STATS<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+botname = Config.TG_BOT_USER_NAME_BF_HER
+pvtgrpid = Config.PRIVATE_GROUP_BOT_API_ID	
+sudousers = Config.SUDO_USERS
+lydiaactive =  Var.LYDIA_API_KEY
+inboxsecurity = Var.INBOXSECURITY
+herokuappstats = Var.HEROKU_APP_NAME
+youtubesearch = Config.YOUTUBE_API_KEY
+#>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>TEST SWITCHES<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+if botname: 
+ inlinebot = "Enabled"
 else:
-    log = "Disabled"
+ inlinebot = "Disabled"
 
-if Config.TG_BOT_USER_NAME_BF_HER:
-    bots = "Enabled"
+
+if pvtgrpid:
+ logs = "Enabled"
 else:
-    bots = "Disabled"
+ logs = "Disabled"
 
-if Var.LYDIA_API_KEY:
-    lyd = "Enabled"
+
+if sudousers:
+ sudo = "Disabled"
 else:
-    lyd = "Disabled"
+ sudo = "Enabled"
 
-if Config.SUDO_USERS:
-    sudo = "Disabled"
+
+if lydiaactive:
+ lydia = "Enabled"
 else:
-    sudo = "Enabled"
+ lydia = "Disabled"
 
-if Var.INBOXSECURITY.lower() == "off":
-    pm = "Disabled"
+
+if herokuappstats:
+ herokuapp = "Active"
 else:
-    pm = "Enabled"
+ herokuapp = "Inactive"
 
-TELEUSER = str(ALIVE_NAME) if ALIVE_NAME else "@TeleBotSupport"
+if youtubesearch:
+ yts = "Enabled"
+else:
+ yts = "Disabled"  
+ 
+		
+#>>>>>>>>>>>SPARKZZZ-BOT INLINESTATS>>>>>>>>>>>>>>>>>>>>>>>>>
 
-tele = f"TeleBot Version: {sparkzzzver}\n"
-tele += f"Log Group: {log}\n"
-tele += f"Assistant Bot: {bots}\n"
-tele += f"Lydia: {lyd}\n"
-tele += f"Sudo: {sudo}\n"
-tele += f"PMSecurity: {pm}\n"
-tele += f"\nVisit @TeleBotSupport for assistance.\n"
-telestats = f"{tele}"
+inli  =f"SPARKZZZ Version: {sparkzzzver}\n"
+inli  +=f"INLINE BOT: {inlinebot}\n"
+inli  +=f"HEROKU STATS: {herokuapp}\n"
+inli  +=f"SPARKZZZ LOGS:{logs}\n"
+inli  +=f"LYDIA: {lydia}\n"
+inli  +=f"SUDO: {sudo}\n"
+inli  +=f"YT SEARCH: {yts}\n"
+inli  +=f"\n[SPARKZZZ](t.me/sparkzzzbothelp).\n"
+sparkzzzstats = (f"{inli}")
+
+#>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
