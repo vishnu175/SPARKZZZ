77a671b6868442912254f5c7fc299b4c12b43c32
VISHNU CS <mailvishnucs@gmail.com>
2020-10-20 14:45:58 +0530

    Update whosnum.py

--- userbot/plugins/whosnum.py
+++ userbot/plugins/whosnum.py
@@ -1,37 +1,37 @@
 # (C) SPARKZZZ 2020
-import datetime
 from telethon import events
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.functions.account import UpdateNotifySettingsRequest
-from userbot.utils import admin_cmd
 import asyncio
+#from userbot.utils import admin_cmd
+from userbot.events import register 
+from userbot import bot, CMD_HELP
+from userbot.utils import admin_cmd
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+import os
 
-@sparkzzz.on(admin_cmd(pattern=("num ?(.*)")))
-async def _(event):
-    if event.fwd_from:
-        return 
-    if not event.reply_to_msg_id:
-       await event.edit("```Reply to any 10 digit mobile number.```")
-       return
-    reply_message = await event.get_reply_message() 
-    if not reply_message.text:
-       await event.edit("```reply to a mobile number```")
-       return
+@sparkzzz.on(admin_cmd(outgoing=True, pattern="num(?: |$)(.*)"))
+async def num(num):
+    if num.fwd_from:
+        return
+    d_link = num.pattern_match.group(1)
+    if ".com" not in d_link:
+        await num.edit("`SEND ME A 10 DIGIT MOBILE NUMBER.`**(._.)**")
+    else:
+        await num.edit("**SEARCHING...!**")
     chat = "@knowhobot"
-    sender = reply_message.sender
-    if reply_message.sender.bot:
-       await event.edit("```Reply to a valid mobile number.```")
-       return
-    await event.edit("```Processing```")
-    async with borg.conversation(chat) as conv:
-          try:     
-              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1115604796))
-              await borg.forward_messages(chat, reply_message)
-              response = await response 
-          except YouBlockedUserError: 
-              await event.reply("```you blocked @knowhobot```")
+    async with bot.conversation(chat) as conv:
+          try:
+              msg_start = await conv.send_message("/start")
+              response = await conv.get_response()
+              r = await conv.get_response()
+              msg = await conv.send_message(d_link)
+              details = await conv.get_response()
+              number = await conv.get_response()
+              """ - don't spam notif - """
+              await bot.send_read_acknowledge(conv.chat_id)
+          except YouBlockedUserError:
+              await num.edit("**Error:** `unblock` @knowhobot `and retry!`")
               return
-          if response.text.startswith("Hello,"):
-             await event.edit("```can you kindly disable your forward privacy settings for good?```")
-          else: 
-             await event.edit(f"{response.message.message}")
+          await bot.send_file(num.chat_id, number, caption=details.text)
+          await num.client.delete_messages(conv.chat_id,
+                                             [msg_start.id, response.id, r.id, msg.id, details.id, number.id])
+          await num.delete()          
