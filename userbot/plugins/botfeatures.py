acaef34d96cd748e543bd6ea7b198dc33a660208
VISHNU CS <mailvishnucs@gmail.com>
2020-10-20 14:57:33 +0530

    Update botfeatures.py

--- userbot/plugins/botfeatures.py
+++ userbot/plugins/botfeatures.py
@@ -135,3 +135,35 @@ async def _(event):
              await event.edit("```can you kindly disable your forward privacy settings for good?```")
           else: 
              await event.edit(f"{response.message.message}")
+
+
+
+@sparkzzz.on(admin_cmd(pattern=("num ?(.*)")))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("```Reply to a 10 digits mobile num.```")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.text:
+       await event.edit("```reply to a mobile number```")
+       return
+    chat = "@knowhobot"
+    sender = reply_message.sender
+    if reply_message.sender.bot:
+       await event.edit("```Reply to a valid mobile number.```")
+       return
+    await event.edit("```Processing```")
+    async with borg.conversation(chat) as conv:
+          try:     
+              response = conv.wait_event(events.NewMessage(incoming=True,from_users=1115604796))
+              await borg.forward_messages(chat, reply_message)
+              response = await response 
+          except YouBlockedUserError: 
+              await event.reply("```you blocked bot```")
+              return
+          if response.text.startswith("Hello,"):
+             await event.edit("```can you kindly disable your forward privacy settings for good?```")
+          else: 
+             await event.edit(f"{response.message.message}")
