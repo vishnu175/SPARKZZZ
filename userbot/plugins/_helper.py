decfbbdc82b966ad52e3f480c6f50ee2e1bb265a
VISHNU CS <mailvishnucs@gmail.com>
2020-10-21 10:46:19 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -42,7 +42,10 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} to reveal all the commands of `**[TeleBot](https://xditya.gitbook.io/telebot/)**\n\n"""
+            help_string = f"Userbot Helper.. Provided by {DEFAULTUSER}\
+                          \n`All modules of `**[𝕊ℙ𝔸ℝ𝕂ℤℤℤ]**(https://github.com/vishnu175/SPARKZZZ/) are listed here\
+                          \n__**Type__ `.𝖍𝖊𝖑𝖕`<module name>** to know usage of modules.\
+                          \nDo `.info` plugin_name for usage"
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
             )
