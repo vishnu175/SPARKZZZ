9ad1044ac49d09c4268ed34d213b53110c240b53
VISHNU CS <mailvishnucs@gmail.com>
2020-10-21 12:00:09 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -14,7 +14,7 @@ async def cmd_list(event):
         if tgbotusername is None or input_str == "text":
             string = ""
             for i in CMD_LIST:
-                string += "⚡ " + i + "\n"
+                string += "🌀 " + i + "\n"
                 for iter_list in CMD_LIST[i]:
                     string += "    `" + str(iter_list) + "`"
                     string += "\n"
@@ -44,7 +44,8 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""`Userbot Helper for {DEFAULTUSER} showing all the commands of `**[𝕊ℙ𝔸ℝ𝕂ℤℤℤ](https://github.com/vishnu175/SPARKZZZ/)**\n\n"""
+            help_string = f"""`Userbot Helper for {DEFAULTUSER} showing all the commands of `**[𝕊ℙ𝔸ℝ𝕂ℤℤℤ](https://github.com/vishnu175/SPARKZZZ/)**\n"""
+                          f"""__**Type__ `.𝖍𝖊𝖑𝖕`<module name>** to know usage of modules.\n\n"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
             )
