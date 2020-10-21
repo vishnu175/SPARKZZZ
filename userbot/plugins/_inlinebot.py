77464a8a91285290530a8e1d2cffdbb45b29ed6d
VISHNU CS <mailvishnucs@gmail.com>
2020-10-21 16:19:46 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -63,7 +63,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                     ],
                 ],
             )
-        elif event.query.user_id == bot.uid and query.startswith("**PM"):
+        elif event.query.user_id == bot.uid and query.startswith("**INBOX"):
             SPARKZ = USER_BOT_NO_WARN.format(DEFAULTUSER, myid, MESAG)
             result = builder.photo(
                 file=SECPIC,
