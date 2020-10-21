d491a68b92dacb689c0c2515a8286434e96cede2
VISHNU CS <mailvishnucs@gmail.com>
2020-10-21 17:51:02 +0530

    inbox security inline buttons added

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -1,81 +1,99 @@
-import io
-import math
+#    SPARKZZZ - UserBot
+#    Copyright (C) SPARKZZZ 2020
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import html
+import os
 import re
-from telethon import Button, custom, events
-from userbot import CMD_LIST
-from . import telealive
+from math import ceil
+from telethon import Button, custom, events, functions
+from telethon.tl.functions.users import GetFullUserRequest
+from heroku_config import Var
+from userbot import ALIVE_NAME, CMD_LIST, CUSTOM_PMPERMIT, bot
+from userbot.plugins import sparkzzzstats
 
 
-BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)\]\<buttonurl:(?:/{0,2})(.+?)(:same)?\>)")
+PMPERMIT_PIC = os.environ.get("PMPERMIT_PIC", None)
+SECPIC = (
+    PMPERMIT_PIC
+    if PMPERMIT_PIC
+    else "https://telegra.ph/file/f6a50188e7c0a822e6056.jpg"
+)
+PM_WARNS = {}
+PREV_REPLY_MESSAGE = {}
+myid = bot.uid
+LOG_ID = Var.PRIVATE_GROUP_ID
+MESAG = (
+    str(CUSTOM_PMPERMIT)
+    if CUSTOM_PMPERMIT
+    else "`Dont spam here..else you will be blocked automatically!!!"
+)
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SPARKZZZ User"
+USER_BOT_WARN_ZERO = "`Hey you,..I have already warned you not to spam inbox ✉️. Now you have been blocked and reported until further notice.`\n\n**GoodBye🙋!**" 
+USER_BOT_NO_WARN = (
+    f"**Welcome to ⚡SPARKZZZ inbox security 🔐.**\n\nNice to see you here.unfortunately"
+    f"[{DEFAULTUSER}](tg://user?id={myid}) is not available right now.This is an automated message from SPARKZZZ-BOT inbox security.kindly wait till my master approves  you..or tag him in group\n\n"
+    f"{MESAG}"
+    "\n\n Please select Help menu for further appointment..Thank You."
+)
 
 if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
-    @tgbot.on(events.InlineQuery)
+    @tgbot.on(events.InlineQuery)  # pylint:disable=E0602
     async def inline_handler(event):
         builder = event.builder
         result = None
         query = event.text
-        if query.startswith("**SPARKZZZ") and event.query.user_id == bot.uid:
-            buttons = [
-                (
-                    custom.Button.inline("Stats", data="stats"),
-                    Button.url("Repo", "https://github.com/vishnu175/SPARKZZZ"),
-                )
-            ]
-            result = builder.article(
-                # catpic,
-                title="Alive sparkzzz",
-                # force_document = False,
-                text=query,
-                buttons=buttons,
-            )
-            await event.answer([result] if result else None)
-        elif event.query.user_id == bot.uid and query.startswith("Userbot"):
+        if event.query.user_id == bot.uid and query.startswith("`Userbot"):
             rev_text = query[::-1]
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
-                "© Userbot Help",
+                "© SPARKZZZ Help",
                 text="{}\nCurrently Loaded Plugins: {}".format(query, len(CMD_LIST)),
                 buttons=buttons,
                 link_preview=False,
             )
-            await event.answer([result] if result else None)
-        elif event.query.user_id == bot.uid and query.startswith("Inline buttons"):
-            markdown_note = query[14:]
-            prev = 0
-            note_data = ""
-            buttons = []
-            for match in BTN_URL_REGEX.finditer(markdown_note):
-                # Check if btnurl is escaped
-                n_escapes = 0
-                to_check = match.start(1) - 1
-                while to_check > 0 and markdown_note[to_check] == "\\":
-                    n_escapes += 1
-                    to_check -= 1
-                # if even, not escaped -> create button
-                if n_escapes % 2 == 0:
-                    # create a thruple with button label, url, and newline
-                    # status
-                    buttons.append(
-                        (match.group(2), match.group(3), bool(match.group(4)))
-                    )
-                    note_data += markdown_note[prev : match.start(1)]
-                    prev = match.end(1)
-                # if odd, escaped -> move along
-                else:
-                    note_data += markdown_note[prev:to_check]
-                    prev = match.start(1) - 1
-            else:
-                note_data += markdown_note[prev:]
-            message_text = note_data.strip()
-            tl_ib_buttons = ibuild_keyboard(buttons)
+        elif event.query.user_id == bot.uid and query == "stats":
             result = builder.article(
-                title="Inline creator",
-                text=message_text,
-                buttons=tl_ib_buttons,
-                link_preview=False,
+                title="Stats",
+                text=f"**SPARKZZZ Stats For [{DEFAULTUSER}](tg://user?id={myid})**\n\n(c) @sparkzzzbothelp",
+                buttons=[
+                    [custom.Button.inline("Stats📲", data="enquirestat")],
+                    [Button.url("Repo⚙️", "https://github.com/vishnu175/SPARKZZZ")],
+                    [
+                        Button.url(
+                            "Deploy🌀",
+                            "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2Fvishnu175%2FSPARKZZZ&template=https%3A%2F%2Fgithub.com%2Fvishnu175%2FSPARKZZZ",
+                        )
+                    ],
+                ],
             )
-            await event.answer([result] if result else None)
+        elif event.query.user_id == bot.uid and query.startswith("**INBOX"):
+            SPARKZ = USER_BOT_NO_WARN.format(DEFAULTUSER, myid, MESAG)
+            result = builder.photo(
+                file=SECPIC,
+                text=SPARKZ,
+                buttons=[
+                    
+                        [custom.Button.inline("To ASK 🗣️something", data="ask")],
+                        [custom.Button.inline("To get HELP 🙏", data="wanthelp")],
+                        [custom.Button.inline("To SPAM inbox 📬 ", data="spaminbox")],
+                        [custom.Button.inline("⚡SPARKZZZ USERBOT⚡", data="sparkzzzinfo")],
+                ],
+            )
+        await event.answer([result] if result else None)
 
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -90,9 +108,85 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             await event.edit(buttons=buttons)
         else:
             reply_pop_up_alert = (
-                "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
+                "Hey stop ❌ Why are you clicking my bot..Go and make your own ⚡SPARKZZZ⚡ from @sparkzzzbothelp!"
             )
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"sparkzzzinfo")))
+    async def on_pm_click(event):
+        if event.query.user_id == bot.uid:
+            reply_pop_up_alert = "You are my master🧑‍⚖️!! Majesty!This option is not for you.."
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        else:
+            await event.edit(
+                f"SPARKZZZ userbot is a Telegram Userbot created by [CSV1990](t.me/CSV1990).programmed with Telethon..based on python,You can deploy userbot at https://github.com/vishnu175/SPARKZZZ.\n\n © [SPARKZZZ](t.me/sparkzzzbothelp)"
+            )
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"ask")))
+    async def on_pm_click(event):
+        if event.query.user_id == bot.uid:
+            reply_pop_up_alert = "You are my master🧑‍⚖️!! Majesty!This option is not for you.."
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        else:
+            await event.edit(
+                f"Request accepted, `{DEFAULTUSER}` will contact you soon!\n Kindly wait for my **master** to approve you"
+            )
+            target = await event.client(GetFullUserRequest(event.query.user_id))
+            first_name = html.escape(target.user.first_name)
+            if first_name is not None:
+                first_name = first_name.replace("\u2060", "")
+            sendtobot = f"Hi...{DEFAULTUSER}, {first_name} want to ask you something in PM!"
+            await tgbot.send_message(LOG_ID, sendtobot)
+
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"wanthelp")))
+    async def on_pm_click(event):
+        if event.query.user_id == bot.uid:
+            reply_pop_up_alert = "You are my master🧑‍⚖️!! Majesty!This option is not for you.."
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        else:
+            await event.edit(
+                f"Okay..Okay..👍.cool..{DEFAULTUSER} will help you...\nType your message in **singleline**...about what help you want"
+            )
+            target = await event.client(GetFullUserRequest(event.query.user_id))
+            first_name = html.escape(target.user.first_name)
+            if first_name is not None:
+                first_name = first_name.replace("\u2060", "")
+            sendtobot = f"Hey {DEFAULTUSER}, {first_name} want a help from you..in PM!"
+            await tgbot.send_message(LOG_ID, sendtobot)
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"spaminbox")))
+    async def on_pm_click(event):
+        if event.query.user_id == bot.uid:
+            reply_pop_up_alert = "You are my master🧑‍⚖️!! Majesty!This option is not for you.."
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+        else:
+            await event.edit(
+                f"So you are here to spam my inbox..⚡SPARKZZZ⚡ SECURITY identified you as a spammer🧟\n\nGoodbye...🙋\n"
+            )
+            await borg(functions.contacts.BlockRequest(event.query.user_id))
+            target = await event.client(GetFullUserRequest(event.query.user_id))
+            first_name = html.escape(target.user.first_name)
+            if first_name is not None:
+                first_name = first_name.replace("\u2060", "")
+            first_name = html.escape(target.user.first_name)
+            await tgbot.send_message(
+                LOG_ID,
+                f"Hey Master,{first_name} tried to spam your inbox.\n So,I **kicked off** the spammer at the very moment",
+            )
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
+    async def on_plug_in_callback_query_handler(event):
+        if event.query.user_id == bot.uid:
+            await event.edit("Help Menu Closed.")
+        else:
+            reply_pop_up_alert = "Hey stop ❌ Why are you clicking my bot ?? Go and make your own ⚡SPARKZZZ⚡ from @sparkzzzbothelp!"
+            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+
+    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"enquirestat")))
+    async def rip(event):
+        text = sparkzzzstats
+        await event.answer(text, alert=True)
 
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -108,22 +202,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             # https://t.me/TelethonChat/115200
             await event.edit(buttons=buttons)
         else:
-            reply_pop_up_alert = (
-                "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
-            )
+            reply_pop_up_alert = "Hey stop ❌ why are you clicking my bot ?? Go and make your own ⚡SPARKZZZ⚡,from @sparkzzzbothelp!"
             await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"secret_(.+?)_(.+)")))
-    async def on_plug_in_callback_query_handler(event):
-        userid = event.pattern_match.group(1)
-        ids = [int(userid), bot.uid]
-        if event.query.user_id in ids:
-            encrypted_tcxt = event.pattern_match.group(2)
-            reply_pop_up_alert = encrypted_tcxt
-        else:
-            reply_pop_up_alert = "You little shit, why are you looking at this ? Go away and do your own work,idiot"
-
-        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
 
     @tgbot.on(
         events.callbackquery.CallbackQuery(  # pylint:disable=E0602
@@ -144,99 +224,48 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 reply_pop_up_alert = "{} is useless".format(plugin_name)
             else:
                 reply_pop_up_alert = help_string
-            reply_pop_up_alert += (
-                "Use .unload {} to remove this plugin ©SPARKZZZ userbot".format(plugin_name)
+            reply_pop_up_alert += "\n Use .unload {} to remove this plugin\n\
+                © SPARKZZZ".format(
+                plugin_name
             )
             try:
                 await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
             except BaseException:
-                # https://github.com/Dark-Princ3/X-tra-Telegram/commit/275fd0ec26b284d042bf56de325472e088e6f364#diff-2b2df8998ff11b6c15893b2c8d5d6af3
-                with io.BytesIO(str.encode(reply_pop_up_alert)) as out_file:
-                    out_file.name = "{}.txt".format(plugin_name)
-                    await borg.send_file(
-                        event.chat_id,
-                        out_file,
-                        force_document=True,
-                        allow_cache=False,
-                        caption=plugin_name,
-                    )
+                halps = "Do .help {} to get the list of commands.".format(plugin_name)
+                await event.answer(halps, cache_time=0, alert=True)
         else:
-            reply_pop_up_alert = (
-                "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
-            )
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"close")))
-    async def on_plug_in_callback_query_handler(event):
-        if event.query.user_id == bot.uid:
-            await event.edit("menu closed")
-        else:
-            reply_pop_up_alert = (
-                "Aary bas kar Bhai !! Tab se dabate jaa rha h, Khudka bot bana!"
-            )
-            await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
-
-    @tgbot.on(events.callbackquery.CallbackQuery(data=re.compile(b"stats")))
-    async def on_plug_in_callback_query_handler(event):
-        statstext = await catalive()
-        reply_pop_up_alert = statstext
-        await event.answer(reply_pop_up_alert, cache_time=0, alert=True)
+            reply_pop_up_alert = "Hey stop ❌ Why are you clicking my bot ?? Go and make your own ⚡SPARKZZZ⚡ from @sparkzzzbothelp!"
 
 
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows = Config.NO_OF_INLINE_ROWS
-    number_of_cols = Config.NO_OF_INLINE_COLUMNS
-    helpable_plugins = [p for p in loaded_plugins if not p.startswith("_")]
+    number_of_rows = 8
+    number_of_cols = 3
+    helpable_plugins = []
+    for p in loaded_plugins:
+        if not p.startswith("_"):
+            helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
-    modules = [
-        custom.Button.inline(
-            "{} {} {}".format(
-                Config.INLINE_EMOJI, x, Config.INLINE_EMOJI
-            ),
-            data="us_plugin_{}".format(x),
-        )
-        for x in helpable_plugins
-    ]
-    if number_of_cols == 1:
-        pairs = list(zip(modules[::number_of_cols]))
-    elif number_of_cols == 2:
-        pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
-    else:
-        pairs = list(
-            zip(
-                modules[::number_of_cols],
-                modules[1::number_of_cols],
-                modules[2::number_of_cols],
-            )
-        )
+    modules = [custom.Button.inline(
+        "{} {} {}".format(Config.INLINE_EMOJI, x, Config.INLINE_EMOJI),
+        data="us_plugin_{}".format(x))
+        for x in helpable_plugins]
+    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols], modules[2::number_of_cols]))
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
-    elif len(modules) % number_of_cols == 2:
-        pairs.append((modules[-2], modules[-1]))
-    max_num_pages = math.ceil(len(pairs) / number_of_rows)
+    max_num_pages = ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
-        pairs = pairs[
-            modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)
-        ] + [
-            (
-                custom.Button.inline(
-                    "👈Previous", data="{}_prev({})".format(prefix, modulo_page)
-                ),
-                custom.Button.inline("⚡Close⚡", data="close"),
-                custom.Button.inline(
-                    "Next👉", data="{}_next({})".format(prefix, modulo_page)
-                ),
-            )
+        pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
+            [
+            (custom.Button.inline("👈Previous", data="{}_prev({})".format(prefix, modulo_page)),
+             custom.Button.inline("⚡Close⚡", data="close"),
+             custom.Button.inline("Next👉", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
 
-
-def ibuild_keyboard(buttons):
-    keyb = []
-    for btn in buttons:
-        if btn[2] and keyb:
-            keyb[-1].append(Button.url(btn[0], btn[1]))
-        else:
-            keyb.append([Button.url(btn[0], btn[1])])
-    return keyb
+async def userinfo(event):
+    target = await event.client(GetFullUserRequest(event.query.user_id))
+    first_name = html.escape(target.user.first_name)
+    if first_name is not None:
+        first_name = first_name.replace("\u2060", "")
+    return first_name
