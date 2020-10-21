9a1a41848512c8fa50ca6cd9a0c150f2380c22a8
VISHNU CS <mailvishnucs@gmail.com>
2020-10-21 11:24:33 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -222,7 +222,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
 
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows = 8
+    number_of_rows = 10
     number_of_cols = 3
     helpable_plugins = []
     for p in loaded_plugins:
@@ -230,28 +230,44 @@ def paginate_help(page_number, loaded_plugins, prefix):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [
-        custom.Button.inline("{} {} {}".format("🔥", x, "🔥"),
-                             data="us_plugin_{}".format(x))
+        custom.Button.inline(
+            "{} {} {}".format(
+                Config.INLINE_EMOJI, x, Config.INLINE_EMOJI
+            ),
+            data="us_plugin_{}".format(x),
+        )
         for x in helpable_plugins
     ]
-    pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols], modules[2::number_of_cols]))
+    if number_of_cols == 1:
+        pairs = list(zip(modules[::number_of_cols]))
+    elif number_of_cols == 2:
+        pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
+    else:
+        pairs = list(
+            zip(
+                modules[::number_of_cols],
+                modules[1::number_of_cols],
+                modules[2::number_of_cols],
+            )
+        )
     if len(modules) % number_of_cols == 1:
-        pairs.append((modules[-1], ))
-    max_num_pages = ceil(len(pairs) / number_of_rows)
+        pairs.append((modules[-1],))
+    elif len(modules) % number_of_cols == 2:
+        pairs.append((modules[-2], modules[-1]))
+    max_num_pages = math.ceil(len(pairs) / number_of_rows)
     modulo_page = page_number % max_num_pages
     if len(pairs) > number_of_rows:
         pairs = pairs[
-            modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)
+            modulo_page * number_of_rows : number_of_rows * (modulo_page + 1)
         ] + [
             (
                 custom.Button.inline(
-                    "⏪Previous", data="{}_prev({})".format(prefix, modulo_page)
+                    "👈Previous", data="{}_prev({})".format(prefix, modulo_page)
                 ),
-                custom.Button.inline("⚡Close⚡", data="close"
-                ),                                     
+                custom.Button.inline("⚡Close⚡", data="close"),
                 custom.Button.inline(
-                    "Next⏩", data="{}_next({})".format(prefix, modulo_page)
-                )
+                    "Next👉", data="{}_next({})".format(prefix, modulo_page)
+                ),
             )
         ]
     return pairs
