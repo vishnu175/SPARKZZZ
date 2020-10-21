995fc05a2b2add7665f388b307be2ca26e87024c
VISHNU CS <mailvishnucs@gmail.com>
2020-10-21 08:13:23 +0530

    Update inboxsecurity.py

--- userbot/plugins/inboxsecurity.py
+++ userbot/plugins/inboxsecurity.py
@@ -21,15 +21,14 @@ MESAG = (
     if CUSTOM_PMPERMIT
     else "`Dont spam Here,else you will be blocked Automatically"
 )
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "SPARKZZZ User"
-USER_BOT_WARN_ZERO = "`Hey you,..I have already warned you not to spam inbox ✉️. Now you have been blocked and reported until further notice.`\n\n**GoodBye🙋!**" 
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TeleBot User"
+USER_BOT_WARN_ZERO = "`I had warned you not to spam. Now you have been blocked and reported until further notice.`\n\n**GoodBye!** "
 USER_BOT_NO_WARN = (
-    f"**Welcome to ⚡SPARKZZZ inbox security 🔐.**\n\nNice to see you here.unfortunately"
-    f"[{DEFAULTUSER}](tg://user?id={myid}) is not available right now.This is an automated message from SPARKZZZ-BOT inbox security.kindly wait till my master approves  you..or tag him in group\n\n"
-    f"{MESAG}"
-    "\n\n Please select Help menu for further appointment..Thank You."
+    "**PM Security ~ TeleBot**\n\nNice to see you here, but  "
+    "[{}](tg://user?id={}) is currently unavailable.\nThis is an automated message.\n\n"
+    "{}\n\n**You have** `{}/{}` **warnings...**"
+    "\n\n   ~ Thank You."
 )
-
 
 @sparkzzz.on(admin_cmd(pattern="approve ?(.*)"))
 async def approve_p_m(event):
