3660873c88e03c6985b9f15facce94b311ce98e7
VISHNU CS <mailvishnucs@gmail.com>
2020-11-09 10:52:23 +0530

    Update gdance.py

--- userbot/plugins/gdance.py
+++ userbot/plugins/gdance.py
@@ -9,7 +9,7 @@ from telethon import events
 @borg.on(admin_cmd(pattern="gdance ?(.*)"))
 async def _(event):
      if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("connecting to server....")
+        await event.edit("connecting to SPARKZZZ server....")
         await asyncio.sleep(2)
         await event.edit("⠀⠀⠀⣶⣿⣶\n⠀⠀⠀⣿⣿⣿⣀\n⠀⣀⣿⣿⣿⣿⣿⣿\n⣶⣿⠛⣭⣿⣿⣿⣿\n⠛⠛⠛⣿⣿⣿⣿⠿\n⠀⠀⠀⠀⣿⣿⣿\n⠀⠀⣀⣭⣿⣿⣿⣿⣀\n⠀⠤⣿⣿⣿⣿⣿⣿⠉\n⠀⣿⣿⣿⣿⣿⣿⠉\n⣿⣿⣿⣿⣿⣿\n⣿⣿⣶⣿⣿\n⠉⠛⣿⣿⣶⣤\n⠀⠀⠉⠿⣿⣿⣤\n⠀⠀⣀⣤⣿⣿⣿\n⠀⠒⠿⠛⠉⠿⣿\n⠀⠀⠀⠀⠀⣀⣿⣿\n⠀⠀⠀⠀⣶⠿⠿⠛\n")
         await asyncio.sleep(2)
