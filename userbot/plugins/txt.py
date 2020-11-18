d3e45e02d0e0f8828e47cc183ef39208470fd08b
VISHNU CS <mailvishnucs@gmail.com>
2020-11-18 10:27:24 +0530

    Create txt.py

---
+++ userbot/plugins/txt.py
@@ -0,0 +1,36 @@
+#keep credit if u gonna edit or kang it 
+#without creadit copy paster mc 
+#creadits to  sawan(@veryhelpful) n
+
+
+import random, re
+from userbot.utils import admin_cmd
+import asyncio
+from telethon import events
+
+@borg.on(admin_cmd(pattern="gst ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("^v^  ^v^  ^v^  ^v^  ^v^  ^v^  ^v^  ^v^  ^v^  ^v^\n┈┈┈┈┈╭̸╮̸┈┈┈┈┈╱▔▔▔╲\n┈┈┈▕▔▔▔▔▏╱╲╱┈Good`  ▕\n┈┈┈┏▏▉▕▊┓▔╲╱'night'╱\n┈┈┈┗▏▅▅▕┛┈┈┈┈▔▔┈\n┈╱▔▔▔▔▔▔▔▔╲┈▕▔▏┈\n╱╱▔▏┏┳┳┓▕▔╲╲╱╱┈┈\n╲╲▕┓┗▅▅┛┏▏┈╲╱┈┈┈\n----------------\n\n----------------")
+
+
+@borg.on(admin_cmd(pattern="gdm ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("^v^  ^v^  ^v^  ^v^  ^v^  ^v^  ^v^  ^v^  ^v^  ^v^\n┈┈┈┈┈╭̸╮̸┈┈┈┈┈╱▔▔▔╲\n┈┈┈▕▔▔▔▔▏╱╲╱┈Good`  ▕\n┈┈┈┏▏▉▕▊┓▔╲╱'morning'╱\n┈┈┈┗▏▅▅▕┛┈┈┈┈▔▔┈\n┈╱▔▔▔▔▔▔▔▔╲┈▕▔▏┈\n╱╱▔▏┏┳┳┓▕▔╲╲╱╱┈┈\n╲╲▕┓┗▅▅┛┏▏┈╲╱┈┈┈\n----------------\n\n----------------")
+
+
+@borg.on(admin_cmd(pattern="fhey ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("╭╮╱╭┳━━━┳╮╱╱╭╮\n┃┃╱┃┃╭━━┫╰╮╭╯┃\n┃╰━╯┃╰━━╋╮╰╯╭╯\n┃╭━╮┃╭━━╯╰╮╭╯\n┃┃╱┃┃╰━━╮╱┃┃\n╰╯╱╰┻━━━╯╱╰╯")
+
+@borg.on(admin_cmd(pattern="hbd ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("╗──────────\n║╚╦═╗╔═╦═╦╦╗\n║║║╬╚╣╬║╬║║║\n╚╩╩══╣╔╣╔╬╗║\n─────╚╝╚╝╚═╝\n╔╗╔╗─╔╗╔╗─╔╗────\n║╚╬╬╦╣╚╣╚╦╝╠═╗╔╦╗\n║╬║║╔╣╔╣║║╬║╬╚╣║║\n╚═╩╩╝╚═╩╩╩═╩══╬╗║\n──────────────╚═╝\n-------------")
+
+@borg.on(admin_cmd(pattern="habd ?(.*)"))
+async def _(event):
+     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        await event.edit("***/\****/\****/\****\n***\/****\/****\/****\n***||****||****||******\n***||****||****||******\n(-----------------)*****\n(-------Happy------)***\n***(-----Birth-day----)***\n***(______________)*** \n----------------")
