bc5f0eeb670fed803206e92d256f1a3c52d8387e
VISHNU CS <mailvishnucs@gmail.com>
2020-11-28 16:32:56 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -286,7 +286,7 @@ def paginate_help(page_number, loaded_plugins, prefix):
         pairs = pairs[modulo_page * number_of_rows:number_of_rows * (modulo_page + 1)] + \
             [
             (custom.Button.inline("🔍Previous", data="{}_prev({})".format(prefix, modulo_page)),
-             custom.Button.inline("🎅Close🎅", data="close"),
+             custom.Button.inline("❌Close❌", data="close"),
              custom.Button.inline("Next🔎", data="{}_next({})".format(prefix, modulo_page)))
         ]
     return pairs
